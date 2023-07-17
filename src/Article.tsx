b56d66dd3d6294bcb79796cf115f6ad3c848eeb4
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-07-17 20:39:53 +0300

    added Article.css

---
+++ src/Article.tsx
@@ -0,0 +1,19 @@
+import './Article.css'
+
+type Props = {
+    title: string
+    description: string
+    author: string
+}
+
+const Article = ({ title, description, author }: Props) => {
+    return (
+        <div>
+            <h2 className="article-title">{title}</h2>
+            <p>{description}</p>
+            <div>Author: {author}</div>
+        </div>
+    )
+}
+
+export default Article
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header'
+import Article from './Article'
 
 const Content = () => {
     return (
@@ -26,6 +27,16 @@ const App = () => {
             <Header title="React" year={2024} />
             <Content />
             <Header title="TS" year={2020} />
+            <Article
+                title="News #1"
+                description="Was written by a 5 y/o"
+                author="Gwen"
+            />
+            <Article
+                title="News #2"
+                description="Was written by a 95 y/o"
+                author="John"
+            />
         </React.Fragment>
     )
 }
