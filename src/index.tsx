0952da04f8181beca1db51a1a7ba2c513874379a
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-07-17 19:25:42 +0300

    React component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//NO JSX
-const title = React.createElement('h1', { id: 'title' }, 'Hello React')
-
-//JSX
-const h1 = <h1 id="title-2">Hello React.js</h1>
+const App = () => {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo libero ratione eveniet tempore corporis, blanditiis
+                perspiciatis quidem. Neque accusamus at, fugit, eos voluptates
+                modi harum quam asperiores repellendus in vitae!
+            </p>
+            <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo libero ratione eveniet tempore corporis, blanditiis
+                perspiciatis quidem. Neque accusamus at, fugit, eos voluptates
+                modi harum quam asperiores repellendus in vitae!
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{h1}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
