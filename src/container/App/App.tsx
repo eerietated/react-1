1020913b00e1464a8d62d343c993ed6cc5114695
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-07-24 19:19:42 +0300

    added Header

---
+++ src/container/App/App.tsx
@@ -0,0 +1,15 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from './Header/Header'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+        </>
+    )
+}
+
+export default App
