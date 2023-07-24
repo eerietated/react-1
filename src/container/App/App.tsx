d6bf12f61c99afa589afb01163e2ff03c9db4ce6
Vitalii Otiuskyi <vitalii.otiuskyi@gmail.com>
2023-07-24 19:35:25 +0300

    added Header.scss

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,14 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline'
-import Header from './Header/Header'
+import Header from '../Header/Header'
+import { StyledEngineProvider } from '@mui/material/styles';
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
-        </>
+        </StyledEngineProvider>
     )
 }
 
